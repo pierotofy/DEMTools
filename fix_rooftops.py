edb7e34eb73fbc176fbfc47376d181aab4b255e2
Piero Toffanin <pt@masseranolabs.com>
2021-04-02 20:19:27 +0000

    Added geometry functions

--- fix_rooftops.py
+++ fix_rooftops.py
@@ -6,6 +6,7 @@ import argparse
 import rasterio
 import fiona
 import numpy as np
+import math
 import numpy.ma as ma
 
 parser = argparse.ArgumentParser(description='Fix rooftops in a DEM')
@@ -16,7 +17,7 @@ parser.add_argument('cutlines',
                 type=str,
                 help='Path to a GDAL-readable vector file containing line strings with rooftop cutlines')
 parser.add_argument('output',
-                    type=bool,
+                    type=str,
                     help="Output DEM")
 
 args = parser.parse_args()
@@ -36,6 +37,152 @@ class GeoPoint:
     def inside_raster(self):
         col, row = self.raster_coords()
         return row >= 0 and col >= 0 and row < dem_raster.height and col < dem_raster.width
+    
+    def normalize(self):
+        length = math.sqrt(self.x ** 2 + self.y ** 2)
+        if length != 0:
+            return GeoPoint(self.x / length, self.y / length)
+        return GeoPoint(0, 0)
+    
+    def __mul__(self, factor):
+        return GeoPoint(self.x * factor, self.y * factor)
+
+    def __add__(self, other):
+        return GeoPoint(self.x + other.x, self.y + other.y)
+
+    def __str__(self):
+        return "(%s %s)" % (self.x, self.y)
+
+class GeoLine:
+    def __init__(self, x1, y1, x2, y2):
+        self.a = GeoPoint(x1, y1)
+        self.b = GeoPoint(x2, y2)
+    
+    def inside_raster(self):
+        return self.a.inside_raster() and self.b.inside_raster()
+
+    def stretch(self, length):
+        length_ab = math.sqrt((self.a.x - self.b.x)**2 + (self.a.y - self.b.y)**2) 
+        f = length_ab * length
+
+        b_x = self.b.x + (self.b.x - self.a.x) / f
+        b_y = self.b.y + (self.b.y - self.a.y) / f
+
+        a_x = self.a.x + (self.a.x - self.b.x) / f
+        a_y = self.a.y + (self.a.y - self.b.y) / f
+
+        return GeoLine(a_x, a_y, b_x, b_y)
+    
+    def compute_normals(self):
+        dx = self.b.x - self.a.x
+        dy = self.b.y - self.a.y
+
+        n1 = GeoPoint(-dy, dx)
+        n2 = GeoPoint(dy, -dx)
+
+        return [n1.normalize(), n2.normalize()]
+
+    def expand_to_box(self, length):
+        n1, n2 = self.compute_normals()
+        n1 = n1 * length
+        n2 = n2 * length
+
+        p1 = (n1 + self.a)
+        p2 = (n1 + self.b)
+        p3 = (n2 + self.b)
+        p4 = (n2 + self.a)
+
+        return GeoRectangle(p1.x, p1.y,
+                        p2.x, p2.y,
+                        p3.x, p3.y,
+                        p4.x, p4.y)
+
+    def buffer_rectangle(self, length):
+        return self.stretch(length).expand_to_box(length)
+
+    def __str__(self):
+        return "[%s, %s]" % (self.a, self.b)
+
+class GeoRectangle:
+    @staticmethod
+    def triangle_area(a, b, c):
+        return abs((b[0] * a[1] - a[0] * b[1]) + (c[0] * b[1] - b[0] * c[1]) + (a[0] * c[1] - c[0] * a[1])) / 2.0
+
+    def __init__(self, *coords):
+        num_coords = len(coords)
+        if num_coords != 8:
+            raise ValueError("Wrong number of coords (%s)" % num_coords)
+        
+        self.coords = []
+        for i in range(0, num_coords - 1, 2):
+            self.coords.append(GeoPoint(coords[i], coords[i + 1]))
+    
+    def inside_raster(self):
+        return all([p.inside_raster() for p in self.coords])
+
+    def raster_mask(self):
+        mask = np.zeros((dem_raster.shape[0], dem_raster.shape[1]), dtype=np.bool)
+        bbox = self.bbox().raster_box()
+
+        for y in range(mask.shape[0]):
+            for x in range(mask.shape[1]):
+                if bbox.contains(x, y):
+                    if self.contains_raster_coords(x, y):
+                        mask[y][x] = True
+
+        return mask
+
+    def raster_coords(self):
+        return list(map(GeoPoint.raster_coords, self.coords))
+    
+    def contains_raster_coords(self, x, y):
+        a, b, c, d = self.raster_coords()
+        p = (x, y)
+
+        total_area = self.triangle_area(a, p, d) + \
+                     self.triangle_area(d, p, c) + \
+                     self.triangle_area(c, p, b) + \
+                     self.triangle_area(p, b, a)
+        rect_area = self.triangle_area(a, b, d) + self.triangle_area(b, c, d)
+        if total_area <= rect_area:
+            return True
+
+        return False
+
+    def bbox(self):
+        x_coords = [p.x for p in self.coords]
+        y_coords = [p.y for p in self.coords]
+
+        return GeoBox(GeoPoint(min(x_coords), min(y_coords)),
+                      GeoPoint(max(x_coords), max(y_coords)))
+
+    def __str__(self):
+        return "[" + ", ".join(map(str, self.coords))  + "]"
+
+class GeoBox:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def raster_box(self):
+        rp1 = self.p1.raster_coords()
+        rp2 = self.p2.raster_coords()
+
+        return RasterBox(rp1[0], rp1[1], rp2[0], rp2[1])
+
+class RasterBox:
+    def __init__(self, x1, y1, x2, y2):
+        self.minx = min(x1, x2)
+        self.miny = min(y1, y2)
+        self.maxx = max(x1, x2)
+        self.maxy = max(y1, y2)
+    
+    def contains(self, x, y):
+        return x >= self.minx and x <= self.maxx and \
+               y >= self.miny and y <= self.maxy
+
+    def __str__(self):
+        return "[(%s, %s), (%s, %s)]" % (self.minx, self.miny, self.maxx, self.maxy)
 
 if not dem_raster.crs:
     print("DEM has no CRS (we need that!)")
@@ -48,6 +195,8 @@ if str(dem_raster.crs) != cutlines_vector.crs.get('init').upper():
     print("CRSes do not match!")
     exit(1)
 
+dem = dem_raster.read()[0]
+
 for cutline in cutlines_vector:
     geom = cutline.get('geometry')
     line_id = geom.get('id')
@@ -56,18 +205,37 @@ for cutline in cutlines_vector:
         print("Skipping feature %s (not linestring)" % line_id)
         continue
     
-    line = geom.get('coordinates')
+    line_coords = geom.get('coordinates')
 
     # For each line segment
-    for i in range(len(line) - 1):
-        a, b = GeoPoint(*line[i]), GeoPoint(*line[i + 1])
+    for i in range(len(line_coords) - 1):
+        line = GeoLine(*line_coords[i], *line_coords[i + 1])
+        buffer = line.buffer_rectangle(1)
 
-        if a.inside_raster() and b.inside_raster():
-            print(a.raster_coords(), b.raster_coords())
+        if buffer.inside_raster():
+            # bbox = buffer.bbox()
+            mask = buffer.raster_mask()
 
+            dem[mask] = 0
+
+            profile = {
+                'driver': 'GTiff',
+                'width': dem.shape[1],
+                'height': dem.shape[0],
+                'count': 1,
+                'dtype': dem.dtype.name,
+                'transform': dem_raster.transform,
+                'nodata': dem_raster.nodata,
+                'crs': dem_raster.crs
+            }
+
+            with rasterio.open(args.output, 'w', **profile) as wout:
+                wout.write(dem, 1)
+
+            exit(1)
             print("TODO: extract buffer window")
         else:
-            print("Line %s outside of DEM raster bounds" % line_id)
+            print("Buffered line %s outside of DEM raster bounds" % line_id)
 
 
 
